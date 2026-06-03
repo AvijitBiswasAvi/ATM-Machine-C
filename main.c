e71f63b516ec4befac0f7b8a0a64a0f8f9918488
AvijitBiswasAvi <aviavijitbiswas35@gmail.com>
2026-06-03 10:17:27 +0600

    add menu

--- main.c
+++ main.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
 
-int main() {
-    
-    
+int main()
+{
+    int choice;
+
+    printf("|========================|\n");
+    printf("|         MENU           |\n");
+    printf("|------------------------|\n");
+    printf("| 1. Check Balance       |\n");
+    printf("| 2. Deposit Money       |\n");
+    printf("| 3. Withdraw Money      |\n");
+    printf("| 4. Exit                |\n");
+    printf("|========================|\n");
+
+    printf("Enter your choice: ");
+    scanf("%d", &choice);
     return 0;
 }

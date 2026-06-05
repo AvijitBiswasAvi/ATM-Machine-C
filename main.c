cbf271f9cbd23b7b84551afbe6588a4ef5cdacf0
AvijitBiswasAvi <aviavijitbiswas35@gmail.com>
2026-06-05 10:41:54 +0600

    update atm machine

--- main.c
+++ main.c
@@ -6,25 +6,98 @@ int withdraw(int);
 
 void checkbalance(int balance)
 {
-    printf("You'r Balance is = $%d\n", balance);
+    printf("Your Balance is = $%d\n", balance);
+}
+int deposit(int balance)
+{
+    int amount;
+    printf("Enter your deposit amount = ");
+    scanf("%d", &amount);
+
+    if (amount < 0)
+    {
+        printf("Please enter higher amount");
+        return balance;
+    }
+    printf("You have deposited = $%d\n", amount);
+    balance += amount;
+    printf("Your current balance = $%d", balance);
+    return balance;
+
+}
+int withdraw(int balance)
+{
+    int amount;
+    printf("Enter your withdraw amount = ");
+    scanf("%d", &amount);
+
+    if (amount <= 0)
+    {
+        printf("Please enter higher amount");
+    }
+    else if (amount > balance)
+    {
+        printf("Withdraw not possible");
+    }
+    else
+    {
+        balance -= amount;
+        printf("Successfully withdraw = $%d\n", amount);
+    }
+    printf("Your current balance = $%d", balance);
+    return balance;
 }
 int main()
 {
     int balance = 1000;
     int choice;
+    int atm_pin = 12345;
+    int user_pin;
 
-    printf("\n");
-    printf("|=====================|\n");
-    printf("|        MENU         |\n");
-    printf("|---------------------|\n");
-    printf("| 1. Check Balance    |\n");
-    printf("| 2. Deposit Money    |\n");
-    printf("| 3. Withdraw Money   |\n");
-    printf("| 4. Exit             |\n");
-    printf("|=====================|\n");
+    printf("Enter your pin = ");
+    scanf("%d", &user_pin);
 
-    printf("Enter your choice = ");
-    scanf("%d", &choice);
+    if (user_pin != atm_pin)
+    {
+        printf("Error ! INVALID PIN");
+    }
+    else
+    {
+        do
+        {
+            printf("\n");
+            printf("|=====================|\n");
+            printf("|        MENU         |\n");
+            printf("|---------------------|\n");
+            printf("| 1. Check Balance    |\n");
+            printf("| 2. Deposit Money    |\n");
+            printf("| 3. Withdraw Money   |\n");
+            printf("| 4. Exit             |\n");
+            printf("|=====================|\n");
+            
+            printf("Enter your choice =  ");
+            scanf("%d", &choice);
+           
 
+            switch (choice)
+            {
+            case 1:
+                checkbalance(balance);
+                break;
+            case 2:
+                balance = deposit(balance);
+                break;
+            case 3:
+                balance = withdraw(balance);
+                break;
+            case 4:
+                printf("Thank you for using our ATM");
+                break;
+            default:
+                printf("INVALID CHOICE");
+                break;
+            }
+        } while (choice != 4);
+    }
     return 0;
 }
